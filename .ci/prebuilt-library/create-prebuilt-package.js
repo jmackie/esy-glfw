e02ea744f1dd1d09b4e164a4b45a644b2d43e1f2
Bryan Phelps <bryphe@outlook.com>
2018-12-21 16:46:59 -0800

    Update ci

---
+++ .ci/prebuilt-library/create-prebuilt-package.js
@@ -0,0 +1,61 @@
+/**
+ * create-prebuilt-package.js
+ *
+ * This script creates the template for a package that has prebuilt binaries.
+ */
+
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = path.join(__dirname, "..", "..")
+
+console.log("Creating package.json");
+const mainPackageJson = require(path.join(rootDir, "package.json"));
+const exportedEnv = mainPackageJson["esy"].exportedEnv || {};
+const packageJson = JSON.stringify(
+    {
+        name: mainPackageJson.name + "-prebuilt",
+        version: mainPackageJson.version,
+        license: mainPackageJson.license,
+        description: mainPackageJson.description,
+        repository: mainPackageJson.repository,
+        scripts: {
+            postinstall: "node ./postinstall.js",
+        },
+        esy: {
+            build: [
+                "cp -r _prebuilt/. $cur__install/"
+                "echo Installed $cur__name to $cur__install"
+            ],
+            exportedEnv: exportedEnv,
+        },
+        files: [
+            "postinstall.js",
+            "platform-linux/",
+            "platform-darwin/",
+            "platform-win32/"
+        ]
+    },
+    null,
+    2
+);
+
+fs.writeFileSync(
+  path.join(rootDir, "_release", "package.json"),
+  packageJson, 
+  {
+    encoding: "utf8"
+  }
+);
+
+console.log("Copying README.md");
+fs.copyFileSync(
+  path.join(rootDir, "README.md"),
+  path.join(rootDir, "_release", "README.md")
+);
+
+console.log("Copying postinstall.js");
+fs.copyFileSync(
+  path.join(__dirname, "prebuilt-postinstall.js"),
+  path.join(rootDir, "_release", "postinstall.js")
+);
---
+++ .ci/prebuilt-library/prebuilt-postinstall.js
@@ -0,0 +1,54 @@
+/**
+ * prebuilt-postinstall.js
+ *
+ * XXX: We want to keep this script installable at least with node 4.x.
+ *
+ * This script is bundled with the `npm` package and executed on release.
+ * Since we have a 'fat' NPM package (with all platform libraries bundled),
+ * this postinstall script extracts them and puts the current platform's
+ * bits in the right place.
+ */
+
+var path = require("path");
+var cp = require("child_process");
+var fs = require("fs");
+var os = require("os");
+var platform = process.platform;
+
+function copyRecursive(srcDir, dstDir) {
+  var results = [];
+  var list = fs.readdirSync(srcDir);
+  var src, dst;
+  list.forEach(function(file) {
+    src = path.join(srcDir, file);
+    dst = path.join(dstDir, file);
+
+    var stat = fs.statSync(src);
+    if (stat && stat.isDirectory()) {
+      try {
+        fs.mkdirSync(dst);
+      } catch (e) {
+        console.log("directory already exists: " + dst);
+        console.error(e);
+      }
+      results = results.concat(copyRecursive(src, dst));
+    } else {
+      try {
+        fs.writeFileSync(dst, fs.readFileSync(src));
+      } catch (e) {
+        console.log("could't copy file: " + dst);
+        console.error(e);
+      }
+      results.push(src);
+    }
+  });
+  return results;
+}
+
+const platform = process.platform;
+
+console.log("Copying prebuilt binaries for: " + process.platform);
+const prebuiltDir = path.join(__dirname, "_prebuilt");
+fs.mkdirSync(prebuiltDir);
+copyRecursive(path.join(__dirname, "platform"), prebuiltDir);
+console.log("Prebuilt binaries copied successfully to: " + prebuiltDir);
